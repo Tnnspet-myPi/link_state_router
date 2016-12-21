c9ddf9fbc0101da0ea29fd0a8d7bf94eab2a90fd
Tnnspet <jnevarez@stevens.edu>
2016-12-20 21:48:53 -0500

    Various Changes
    Update comments and headers throught. Also removed redudent code. Also
    remove dijsktras algorithm and move to router class.

--- Link_State.java
+++ Link_State.java
@@ -2,194 +2,182 @@ package link_state_router;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Link_State
 {
   Map<Integer, Router> routers;
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Link_State                               */
+  /*                                                         */
+  /* Description: Main prototype for Link State              */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public Link_State()
   {
     routers = new HashMap<Integer, Router>();
   }
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Process_File                             */
+  /*                                                         */
+  /* Description: This function takes in a file and          */
+  /*              extracts all necessary data needed to      */
+  /*              put together the network of routers.       */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public void Process_File(File input_file) throws FileNotFoundException
   {
     Scanner scanner = new Scanner(input_file);
     Router current  = null;
+    String[] split;
+    
+    // Continue reading file as long as there is another line.
     while (scanner.hasNextLine()) 
     {
-      String[] split =scanner.nextLine().split(" ");
+      // Split the string up based on spaces. 
+      split = scanner.nextLine().split(" ");
       
+      // If the first part is not empty then we are out router information
       if(!split[0].equals(""))
       {
-        Integer id = Integer.parseInt(split[0]);
-  
-        if(routers.containsKey(id))
+        // Extract the ID of the router and create a temp router
+        Integer id  = Integer.parseInt(split[0]);
+        Router temp = new Router(split[0], split[1],"1");
+        
+        // If user added cost then add it otherwise set cost to 1
+        if(split.length == 3)
         {
-          current = routers.get(id);
-          current.network = split[1];
+          temp.network_cost = Integer.parseInt(split[2]);
+        }
 
-          if(split.length == 3)
-          {
-            current.network_cost = Integer.parseInt(split[2]);
-          }
-          else
-          {
-            current.network_cost = 1;
-          }
-        }
-        else
-        {
-          Router temp;
-          if(split.length == 3)
-          {
-            temp = new Router(split[0], split[1], split[2]);
-          }
-          else
-          {
-            temp = new Router(split[0], split[1],"1");
-          }
-          routers.put(id, temp);
-          current = temp;
-        }
+        // Add routers to our hash map list and set it to current
+        routers.put(id, temp);
+        current = temp;
       }
+      // Here we add direct link information
       else
       {
+        // Parse integer information
         Integer id = Integer.parseInt(split[1]);
-        Router temp;
-        
-        if(routers.containsKey(id))
-        {
-          temp = routers.get(id);
-        }
-        else
-        {
-          temp = new Router(split[1]);
-          routers.put(id, temp);
-        }
-        
         Integer cost = 1; 
+        
+        // If user added cost then set it otherwise remain at 1
         if(split.length == 3)
         {
           cost = Integer.parseInt(split[2]);
         }
 
+        // Add direct link to current router and update routing table
         current.direct_links.put(id, new Pair(cost, 0));
+        current.Update_Routing_Table();
       }
-    }
+    }  // End While loop
+    
+    // Close Scanner
     scanner.close();
-  }
+  }  //End function Process_File
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Print_Routing_Table                      */
+  /*                                                         */
+  /* Description: This function prints the routing table     */
+  /*              for a specific router.                     */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public void Print_Routing_Table(String input)
   {
+    // Change String to id
     Integer id = Integer.parseInt(input);
     if (routers.containsKey(id))
     {
+      // Get current router out of our list
       Router current = routers.get(id);
-      PriorityQueue<Edge> queue = new PriorityQueue<Edge>(new Edge_Cost_Comparator());
-      LinkedList<Edge> edges = new LinkedList<Edge>();
-      Map<Integer, Boolean> visited = new HashMap<Integer, Boolean>();
       
-      for(Map.Entry<Integer, Pair> links : current.direct_links.entrySet())
+      // Ensure the router is updated by running dijkstra algorithm 
+      current.Update_Routing_Table();
+      
+      // Loop through all edges and print to the user 
+      for(Edge s : current.edges)
       {
-        queue.add(new Edge(links.getKey(), links.getValue().Get_Cost(), links.getKey()));
-      }
-      
-      
-      while(!queue.isEmpty())
-      {
-        Edge here = queue.remove();
-        
-        if(visited.containsKey(here.id) || here.cost >= 100000000)
-        {
-          continue;
-        }
-        
-        visited.put(here.id, true);
-        edges.add(here);
-        
-        if(current.link_state_packets.containsKey(here.id))
-        {
-          Packet pack = current.link_state_packets.get(here.id);
-          
-          for(Map.Entry<Integer, Pair> links : pack.link_cost.entrySet())
-          {
-            if(links.getKey() != current.id)
-            {
-              if(!visited.containsKey(links.getKey()))
-              {
-                queue.add(new Edge(links.getKey(),links.getValue().Get_Cost() + here.cost,here.direct_link));
-              }
-            }
-          }
-        }
-      }
-      
-      for(Edge s : edges)
-      {
+        // Get the router to get the network name
+        // and then print to information to the user. 
         Router print = routers.get(s.id);
-        System.out.println(print.network + ", " + s.cost + ", " + s.direct_link);
-      }
+        if(s.cost < 100000000)
+        {
+          System.out.println(print.network + ", " + s.cost + ", " + s.direct_link);
+        }
+        else
+        {
+          System.out.println(print.network + ", Infinity, " + s.direct_link);
+        }
+      }  // End for loop
     }
-  }
+  }  // End function Print_Routing_Table
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Change_Router_State                      */
+  /*                                                         */
+  /* Description: This function changes the state of a       */
+  /*              router if it is on or off.                 */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public void Change_Router_State(String input, boolean new_state)
   {
+    // Get id by parse integer
     Integer id = Integer.parseInt(input);
     
+    // Make sure the router is apart of our list
     if(routers.containsKey(id))
     {
+      // Update its information
       Router temp = routers.get(id);
       temp.network_on = new_state;
     }
-  }
+  }  // End function Change_Router_State
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Continue_Process                         */
+  /*                                                         */
+  /* Description: This function goes through a cycle by      */
+  /*              flooding the network with link state       */
+  /*              packets.                                   */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public void Continue_Process()
   {
     Router current;
+    
+    // Loop through all routers and get them to originate their 
+    // link state packet
     for(Map.Entry<Integer, Router> outer : routers.entrySet())
     {
-      current = outer.getValue();
-      current.Originate_Packet();
+      outer.getValue().Originate_Packet();;
     }
     
+    // Loop through again to have all link state packets 
+    // flood the network
     for(Map.Entry<Integer, Router> outer : routers.entrySet())
     {
+      // Get current router
       current = outer.getValue();
+      
+      // Make sure the router created a link state packet to send
       if(current.current_link_state_packet != null)
       {
+        // Loop through all direct links to send its link state packet
         for(Map.Entry<Integer, Pair> inner : current.direct_links.entrySet())
         {
+          // Make sure the router exist and then send the information
           if(routers.containsKey(inner.getKey()))
           {
-            Router temp = routers.get(inner.getKey());
-            temp.Receive_Packet(current.current_link_state_packet, routers);
+            routers.get(inner.getKey()).Receive_Packet(current.current_link_state_packet, routers);
           }
         }
       }
-    }
-  }
-}
-
-class Edge_Cost_Comparator implements Comparator<Edge>
-{
-  @Override
-  public int compare(Edge x, Edge y)
-  {
-    if(x.cost < y.cost)
-    {
-      return -1;
-    }
-    else if(x.cost > y.cost)
-    {
-      return 1;
-    }
-    return 0;
-  }
-}
+    }  // End for loop
+  }  // End function Continue_Process
+}  // End class Link_State
--- Router.java
+++ Router.java
@@ -1,7 +1,10 @@
 package link_state_router;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class Router
 {
@@ -10,20 +13,36 @@ public class Router
   Integer sequence_num;
   String  network;
   boolean network_on;
-  Map<Integer, Packet> link_state_packets;
-  Map<Integer, Pair> direct_links;
+  Packet  current_link_state_packet;
+  
+  Map<Integer, Packet>  link_state_packets;
+  Map<Integer, Pair>    direct_links;
   Map<Integer, Integer> save_cost;
-  Packet current_link_state_packet;
+  LinkedList<Edge>      edges;
 
+  /*---------------------------------------------------------*/
+  /* Function Name: Router                                   */
+  /*                                                         */
+  /* Description: Prototype for Router class.                */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public Router()
   {
     this.network_on   = true;
     this.direct_links = new HashMap<Integer, Pair>();
     this.sequence_num = 0;
     this.save_cost    = new HashMap<Integer, Integer>();
+    this.edges        = new LinkedList<Edge>();
     this.link_state_packets = new HashMap<Integer, Packet>();
   }
 
+  /*---------------------------------------------------------*/
+  /* Function Name: Router                                   */
+  /*                                                         */
+  /* Description: Prototype for Router class that takes in   */
+  /*              ID.                                        */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public Router(String id)
   {
     this.id           = Integer.parseInt(id);
@@ -32,9 +51,17 @@ public class Router
     this.direct_links = new HashMap<Integer, Pair>();
     this.sequence_num = 0;
     this.save_cost    = new HashMap<Integer, Integer>();
+    this.edges        = new LinkedList<Edge>();
     this.link_state_packets = new HashMap<Integer, Packet>();
   }
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Router                                   */
+  /*                                                         */
+  /* Description: Prototype for Router class that takes in   */
+  /*              ID and Network.                            */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public Router(String id, String network)
   {
     this.id           = Integer.parseInt(id);
@@ -44,9 +71,17 @@ public class Router
     this.direct_links = new HashMap<Integer, Pair>();
     this.sequence_num = 0;
     this.save_cost    = new HashMap<Integer, Integer>();
+    this.edges        = new LinkedList<Edge>();
     this.link_state_packets = new HashMap<Integer, Packet>();
   }
-  
+
+  /*---------------------------------------------------------*/
+  /* Function Name: Router                                   */
+  /*                                                         */
+  /* Description: Prototype for Router class that takes in   */
+  /*              ID and Network and Network Cost.           */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public Router(String id, String network, String network_cost)
   {
     this.id           = Integer.parseInt(id);
@@ -56,24 +91,37 @@ public class Router
     this.direct_links = new HashMap<Integer, Pair>();
     this.sequence_num = 0;
     this.save_cost    = new HashMap<Integer, Integer>();
+    this.edges        = new LinkedList<Edge>();
     this.link_state_packets = new HashMap<Integer, Packet>();
   }
 
+  /*---------------------------------------------------------*/
+  /* Function Name: Receive_Packet                           */
+  /*                                                         */
+  /* Description: This function receives link state          */
+  /*              packets and forwards on as necessary.      */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public void Receive_Packet(Packet input, Map<Integer, Router> routers)
   {
+    // Make sure the router is turned on
     if(network_on == true)
     {
+      // Decrement time to live
       input.time_to_live--;
       
-      // Reset the tick
+      // Reset the tick mark since we received a packet
       if(direct_links.containsKey(input.id))
       {
         Pair pairs = direct_links.get(input.id);
         
+        // If the router was originally turned off then and
+        // now is turned on then put the cost back to normal
         if(save_cost.containsKey(input.id))
         {
           pairs.Set_Cost(save_cost.get(input.id));
           save_cost.remove(input.id);
+          this.Update_Routing_Table();
         }
         pairs.Set_Tick(0);
       }
@@ -84,8 +132,11 @@ public class Router
         return;
       }
 
+      // Check to see if we have received a link state packet
+      // in the past from the same router. 
       if(link_state_packets.containsKey(input.id))
       {
+        // If we have received one then grab the packet
         Packet temp = link_state_packets.get(input.id);
 
         // If we have seen a packet of higher sequence then return
@@ -94,39 +145,146 @@ public class Router
           return;
         }
         
+        // Put the latest link state packet in our memory
         link_state_packets.put(input.id, input);
       }
       else
       {
+        // Put the latest link state packet in our memory
         link_state_packets.put(input.id, input);
       }
       
+      // Loop through all direct links to forward packet along. 
       for(Map.Entry<Integer, Pair> link : direct_links.entrySet())
       {
         Router router = routers.get(link.getKey());
         router.Receive_Packet(input, routers);
-      }
+      }  // End for loop
     }
-  }
+  }  // End function Receive_Packet
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Originate_Packet                         */
+  /*                                                         */
+  /* Description: This function creates a link state         */
+  /*              packet with how it sees the network.       */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public void Originate_Packet()
   {
     current_link_state_packet = null;
     
+    // Make sure the network is on
     if(network_on == true)
     {
+      // Loop through all direct links
       for(Map.Entry<Integer, Pair> my_map : direct_links.entrySet())
       {
+        // Add 1 to the tick level
         my_map.getValue().Set_Tick(my_map.getValue().Get_Tick() + 1);
         
+        // If it has been 2 tick levels then change router to 
+        // 100000000 (infinity) and then update routing table
         if(my_map.getValue().Get_Tick() == 2)
         {
           save_cost.put(my_map.getKey(), my_map.getValue().Get_Cost());
           my_map.getValue().Set_Cost(100000000);
+          this.Update_Routing_Table();
         }
-      }
+      }  // End for Loop
       
       current_link_state_packet = new Packet(id,++sequence_num, direct_links);
     }
+  }  // End function Originate_Packet
+
+  /*---------------------------------------------------------*/
+  /* Function Name: Update_Routing_Table                     */
+  /*                                                         */
+  /* Description: This function uses dijkstras algorithm     */
+  /*              to update its routing table.               */
+  /*                                                         */
+  /*---------------------------------------------------------*/
+  public void Update_Routing_Table()
+  {
+    PriorityQueue<Edge>   queue   = new PriorityQueue<Edge>(new Edge_Cost_Comparator());
+    Map<Integer, Boolean> visited = new HashMap<Integer, Boolean>();
+    
+    // Clear current routing table
+    edges.clear();
+    
+    // Loop through all direct links
+    for(Map.Entry<Integer, Pair> links : direct_links.entrySet())
+    {
+      // Add direct links to priority queue
+      queue.add(new Edge(links.getKey(), links.getValue().Get_Cost(), links.getKey()));
+    }
+    
+    // While queue is not empty
+    while(!queue.isEmpty())
+    {
+      // Grab the lowest costing link
+      Edge here = queue.remove();
+      
+      // If we have visited that ID before then we already
+      // found its lowest costing link so continue on
+      if(visited.containsKey(here.id))
+      {
+        continue;
+      }
+      
+      // If we have not visited the link before then this
+      // Link is the lowest cost to reach it. Add it to 
+      // the routing table and set its visit to true
+      visited.put(here.id, true);
+      edges.add(here);
+      
+      // Now we take all adj edges from the current link
+      // to add its total cost for new edges
+      if(this.link_state_packets.containsKey(here.id))
+      {
+        // Get the direct link information from current edge
+        Packet pack = this.link_state_packets.get(here.id);
+        
+        // Loop through all adjacent edges
+        for(Map.Entry<Integer, Pair> links : pack.link_cost.entrySet())
+        {
+          // Make sure we don't loop back
+          if(links.getKey() != this.id)
+          {
+            // Make sure the edge we want to add has not been found already
+            // to have a lower costing edge
+            if(!visited.containsKey(links.getKey()))
+            {
+              // Add the edge with a new total cost
+              queue.add(new Edge(links.getKey(),links.getValue().Get_Cost() + here.cost,here.direct_link));
+            }
+          }
+        }  // End for loop
+      }
+    }  // End While loop
+  }  // End function Update_Routing_Table
+}  // End Class Router
+
+/*---------------------------------------------------------*/
+/* Class Name: Edge_Cost_Comparator                        */
+/*                                                         */
+/* Description: This class overrides current compare to    */
+/*              create a new Comparator to compare cost.   */
+/*                                                         */
+/*---------------------------------------------------------*/
+class Edge_Cost_Comparator implements Comparator<Edge>
+{
+  @Override
+  public int compare(Edge x, Edge y)
+  {
+    if(x.cost < y.cost)
+    {
+      return -1;
+    }
+    else if(x.cost > y.cost)
+    {
+      return 1;
+    }
+    return 0;
   }
 }

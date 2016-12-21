0953d5a527088e1197081dae33917413a0b6ecf3
Tnnspet <jnevarez@stevens.edu>
2016-12-20 21:49:27 -0500

    Updated Comments
    Updated comments throughout and headers.

--- Packet.java
+++ Packet.java
@@ -10,25 +10,46 @@ public class Packet
   Integer time_to_live;
   Map<Integer, Pair> link_cost;
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Packet                                   */
+  /*                                                         */
+  /* Description: Prototype for Packet class                 */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public Packet()
   {
     this.time_to_live = 10;
     this.link_cost    = new HashMap<Integer, Pair>();
-  }
+  }  // End function Packet
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Packet                                   */
+  /*                                                         */
+  /* Description: Prototype for Packet class that takes in   */
+  /*              ID and sequence number.                    */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public Packet(Integer id, Integer sequence)
   {
     this.id           = id;
     this.sequence     = sequence;
     this.time_to_live = 10;
     this.link_cost    = new HashMap<Integer, Pair>();
-  }
+  }  // End function Packet
   
+  /*---------------------------------------------------------*/
+  /* Function Name: Packet                                   */
+  /*                                                         */
+  /* Description: Prototype for Packet class that takes in   */
+  /*              ID and sequence number and current link    */
+  /*              cost.                                      */
+  /*                                                         */
+  /*---------------------------------------------------------*/
   public Packet(Integer id, Integer sequence, Map<Integer, Pair> current)
   {
     this.id           = id;
     this.sequence     = sequence;
     this.time_to_live = 10;
     this.link_cost    = current;
-  }
-}
+  }  // End function Packet
+}  // End Class Packet
